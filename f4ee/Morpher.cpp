17d449315126bff758b941613b6909bf7376b5e0
WirelessLan <6249587+WirelessLan@users.noreply.github.com>
2026-07-22 23:58:53 +0900

    Optimize duplicate vertex matching

--- f4ee/Morpher.cpp
+++ f4ee/Morpher.cpp
@@ -5,9 +5,11 @@
 #include "half.hpp"
 #include "f4se/BSGeometry.h"
 
-float round_v(float num)
-{
-	return (num > 0.0) ? floor(num + 0.5) : ceil(num - 0.5);
+namespace {
+	float round_v(float num)
+	{
+		return (num > 0.0) ? floor(num + 0.5) : ceil(num - 0.5);
+	}
 }
 
 MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8 * dstBlock, std::function<void(std::vector<Morpher::Vector3> &)> morph) : geometry(_geometry), morphFunc(morph)
@@ -15,28 +17,29 @@ MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8
 	UInt64 vertexDesc = geometry->vertexDesc;
 	UInt32 vertexSize = geometry->GetVertexSize();
 	UInt32 blockSize = geometry->numVertices * vertexSize;
-	BSGeometryData * geomData = geometry->geometryData;
+	BSGeometryData* geomData = geometry->geometryData;
 	UInt32 numVertices = geometry->numVertices;
 
 	// Pull the base data from the vertex block
 	rawVertices.resize(numVertices);
-	if(vertexDesc & BSTriShape::kFlag_UVs)
+	if ((vertexDesc & BSTriShape::kFlag_UVs) == BSTriShape::kFlag_UVs) {
 		rawUV.resize(numVertices);
+	}
 
-	if(vertexDesc & BSTriShape::kFlag_Normals) {
+	if ((vertexDesc & BSTriShape::kFlag_Normals) == BSTriShape::kFlag_Normals){
 		rawNormals.resize(numVertices);
-		if(vertexDesc & BSTriShape::kFlag_Tangents) {
+		if ((vertexDesc & BSTriShape::kFlag_Tangents) == BSTriShape::kFlag_Tangents) {
 			rawTangents.resize(numVertices);
 			rawBitangents.resize(numVertices);
 		}
 	}
 
-	UInt8 * vertexBlock = srcBlock ? srcBlock : geomData->vertexData->vertexBlock;
-	for(UInt32 i = 0; i < numVertices; i++)
+	UInt8* vertexBlock = srcBlock ? srcBlock : geomData->vertexData->vertexBlock;
+	for (UInt32 i = 0; i < numVertices; ++i)
 	{
-		UInt8 * vBegin = &vertexBlock[i * vertexSize];
+		UInt8* vBegin = &vertexBlock[i * vertexSize];
 
-		if(vertexDesc & BSTriShape::kFlag_FullPrecision)
+		if ((vertexDesc & BSTriShape::kFlag_FullPrecision) == BSTriShape::kFlag_FullPrecision)
 		{
 			rawVertices[i].x = (*(float *)vBegin); vBegin += 4;
 			rawVertices[i].y = (*(float *)vBegin); vBegin += 4;
@@ -53,7 +56,7 @@ MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8
 			vBegin += 2; // Skip BitangetX
 		}
 
-		if(vertexDesc & BSTriShape::kFlag_UVs)
+		if ((vertexDesc & BSTriShape::kFlag_UVs) == BSTriShape::kFlag_UVs)
 		{
 			rawUV[i].u = (*(half_float::half *)vBegin); vBegin += 2;
 			rawUV[i].v = (*(half_float::half *)vBegin); vBegin += 2;
@@ -62,20 +65,16 @@ MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8
 
 	morphFunc(rawVertices);
 
-	Morpher::Triangle* triangles = nullptr;
-	auto triangleData = geomData->triangleData;
-	if(triangleData)
-		triangles = (Morpher::Triangle*)triangleData->triangles;
-
+	auto* triangles = (geomData->triangleData != nullptr) ? reinterpret_cast<Morpher::Triangle*>(geomData->triangleData->triangles) : nullptr;
 	RecalcNormals(geometry->numTriangles, triangles);
 	CalcTangentSpace(geometry->numTriangles, triangles);
 	
 	vertexBlock = dstBlock ? dstBlock : geomData->vertexData->vertexBlock;
-	for(UInt32 i = 0; i < numVertices; i++)
+	for (UInt32 i = 0; i < numVertices; ++i)
 	{
-		UInt8 * vBegin = &vertexBlock[i * vertexSize];
+		UInt8* vBegin = &vertexBlock[i * vertexSize];
 
-		if(vertexDesc & BSTriShape::kFlag_FullPrecision)
+		if ((vertexDesc & BSTriShape::kFlag_FullPrecision) == BSTriShape::kFlag_FullPrecision)
 		{
 			(*(float *)vBegin) = rawVertices[i].x; vBegin += 4;
 			(*(float *)vBegin) = rawVertices[i].y; vBegin += 4;
@@ -93,12 +92,12 @@ MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8
 		}
 
 		// Skip UV write
-		if(vertexDesc & BSTriShape::kFlag_UVs)
+		if ((vertexDesc & BSTriShape::kFlag_UVs) == BSTriShape::kFlag_UVs)
 		{
 			vBegin += 4;
 		}
 
-		if(vertexDesc & BSTriShape::kFlag_Normals)
+		if ((vertexDesc & BSTriShape::kFlag_Normals) == BSTriShape::kFlag_Normals)
 		{
 			*(SInt8*)vBegin = (UInt8)round_v((((rawNormals[i].x + 1.0f) / 2.0f) * 255.0f)); vBegin += 1;
 			*(SInt8*)vBegin = (UInt8)round_v((((rawNormals[i].y + 1.0f) / 2.0f) * 255.0f)); vBegin += 1;
@@ -106,7 +105,7 @@ MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8
 
 			*(SInt8*)vBegin = (UInt8)round_v((((rawBitangents[i].y + 1.0f) / 2.0f) * 255.0f)); vBegin += 1;
 
-			if(vertexDesc & BSTriShape::kFlag_Tangents)
+			if ((vertexDesc & BSTriShape::kFlag_Tangents) == BSTriShape::kFlag_Tangents)
 			{
 				*(SInt8*)vBegin = (UInt8)round_v((((rawTangents[i].x + 1.0f) / 2.0f) * 255.0f)); vBegin += 1;
 				*(SInt8*)vBegin = (UInt8)round_v((((rawTangents[i].y + 1.0f) / 2.0f) * 255.0f)); vBegin += 1;
@@ -118,14 +117,14 @@ MorphApplicator::MorphApplicator(BSTriShape * _geometry, UInt8 * srcBlock, UInt8
 	}
 }
 
-void MorphApplicator::RecalcNormals(UInt32 numTriangles, Morpher::Triangle* triangles, const bool smooth, const float smoothThresh)
+void MorphApplicator::RecalcNormals(UInt32 numTriangles, Morpher::Triangle* triangles, bool smooth, float smoothThresh)
 {
-	UInt32 numVertices = rawVertices.size();
+	const auto numVertices = rawVertices.size();
 
 	std::vector<Morpher::Vector3> verts(numVertices);
 	std::vector<Morpher::Vector3> norms(numVertices);
 
-	for (UInt32 i = 0; i < numVertices; i++)
+	for (std::size_t i = 0; i < numVertices; ++i)
 	{
 		verts[i].x = rawVertices[i].x * -0.1f;
 		verts[i].z = rawVertices[i].y * 0.1f;
@@ -134,7 +133,7 @@ void MorphApplicator::RecalcNormals(UInt32 numTriangles, Morpher::Triangle* tria
 
 	// Face normals
 	Morpher::Vector3 tn;
-	for (UInt32 t = 0; t < numTriangles; t++)
+	for (UInt32 t = 0; t < numTriangles; ++t)
 	{
 		triangles[t].trinormal(verts, &tn);
 		norms[triangles[t].p1] += tn;
@@ -142,19 +141,20 @@ void MorphApplicator::RecalcNormals(UInt32 numTriangles, Morpher::Triangle* tria
 		norms[triangles[t].p3] += tn;
 	}
 
-	for (auto &n : norms)
+	for (auto& n : norms) {
 		n.Normalize();
+	}
 
 	// Smooth normals
 	if (smooth) {
-		kd_matcher matcher(verts.data(), numVertices);
-		for (int i = 0; i < matcher.matches.size(); i++)
+		const auto matches = kd_matcher::find_matches(verts);
+		for (std::size_t i = 0; i < matches.size(); ++i)
 		{
-			std::pair<Morpher::Vector3*, int>& a = matcher.matches[i].first;
-			std::pair<Morpher::Vector3*, int>& b = matcher.matches[i].second;
-
+			const auto& a = matches[i].first;
+			const auto& b = matches[i].second;
 			Morpher::Vector3& an = norms[a.second];
 			Morpher::Vector3& bn = norms[b.second];
+
 			if (an.angle(bn) < smoothThresh * DEG2RAD) {
 				Morpher::Vector3 anT = an;
 				an += bn;
@@ -162,11 +162,12 @@ void MorphApplicator::RecalcNormals(UInt32 numTriangles, Morpher::Triangle* tria
 			}
 		}
 
-		for (auto &n : norms)
+		for (auto& n : norms) {
 			n.Normalize();
+		}
 	}
 
-	for (UInt32 i = 0; i < numVertices; i++)
+	for (std::size_t i = 0; i < numVertices; ++i)
 	{
 		rawNormals[i].x = -norms[i].x;
 		rawNormals[i].y = norms[i].z;
@@ -174,28 +175,28 @@ void MorphApplicator::RecalcNormals(UInt32 numTriangles, Morpher::Triangle* tria
 	}
 }
 
-void MorphApplicator::CalcTangentSpace(UInt32 numTriangles, Morpher::Triangle * triangles)
+void MorphApplicator::CalcTangentSpace(UInt32 numTriangles, Morpher::Triangle* triangles)
 {
-	UInt32 numVertices = rawVertices.size();
+	const auto numVertices = rawVertices.size();
 
 	std::vector<Morpher::Vector3> tan1;
 	std::vector<Morpher::Vector3> tan2;
 	tan1.resize(numVertices);
 	tan2.resize(numVertices);
 
-	for (UInt32 i = 0; i < numTriangles; i++)
+	for (UInt32 i = 0; i < numTriangles; ++i)
 	{
 		int i1 = triangles[i].p1;
 		int i2 = triangles[i].p2;
 		int i3 = triangles[i].p3;
 
-		Morpher::Vector3 v1 = rawVertices[i1];
-		Morpher::Vector3 v2 = rawVertices[i2];
-		Morpher::Vector3 v3 = rawVertices[i3];
+		const Morpher::Vector3& v1 = rawVertices[i1];
+		const Morpher::Vector3& v2 = rawVertices[i2];
+		const Morpher::Vector3& v3 = rawVertices[i3];
 
-		Morpher::Vector2 w1 = rawUV[i1];
-		Morpher::Vector2 w2 = rawUV[i2];
-		Morpher::Vector2 w3 = rawUV[i3];
+		const Morpher::Vector2& w1 = rawUV[i1];
+		const Morpher::Vector2& w2 = rawUV[i2];
+		const Morpher::Vector2& w3 = rawUV[i3];
 
 		float x1 = v2.x - v1.x;
 		float x2 = v3.x - v1.x;
@@ -227,7 +228,7 @@ void MorphApplicator::CalcTangentSpace(UInt32 numTriangles, Morpher::Triangle * 
 		tan2[i3] += sdir;
 	}
 
-	for (UInt32 i = 0; i < numVertices; i++)
+	for (std::size_t i = 0; i < numVertices; ++i)
 	{
 		rawTangents[i] = tan1[i];
 		rawBitangents[i] = tan2[i];
@@ -246,10 +247,8 @@ void MorphApplicator::CalcTangentSpace(UInt32 numTriangles, Morpher::Triangle * 
 			rawTangents[i].Normalize();
 
 			rawBitangents[i].Normalize();
-
 			rawBitangents[i] = (rawBitangents[i] - rawNormals[i] * rawNormals[i].dot(rawBitangents[i]));
 			rawBitangents[i] = (rawBitangents[i] - rawTangents[i] * rawTangents[i].dot(rawBitangents[i]));
-
 			rawBitangents[i].Normalize();
 		}
 	}
